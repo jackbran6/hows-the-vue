aaa1430eafa860f4312b0def2f08640bfebd83a8
Jack Branthwaite <jack@formfunfunction.com>
2021-06-29 08:37:41 +1200

    Hamburger menu button implemented

---
+++ src/components/menu-button/MenuButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="menu-container">
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'MenuButton'
+})
+</script>
+
+<style scoped>
+.line {
+  background: #fff;
+  height: 5px;
+  margin-bottom: 5px;
+  width: 60px;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,34 @@
 <template>
-<div class="container">
-  <div class="image">
-    <TitleImage/>
+  <div class="container">
+    <div class="image">
+      <TitleImage />
+    </div>
+    <div class="secondary-container">
+      <MenuButton class="hamburger" />
+      <ContactBox
+        route="contact-us"
+        title="Contact us"
+        weekdays="12pm-11pm"
+        weekend="3pm-11pm"
+        contact="free call 0800 9428787"
+      />
+      <ContactBox
+        route="chat"
+        title="Chat online"
+        weekdays="12pm-10pm"
+        weekend="3pm-10pm"
+        contact="whatsup.co.nz/chat"
+      />
+      <EngageMenu />
+    </div>
   </div>
-  <div class="secondary-container">
-    <ContactBox  route="contact-us" title="Contact us" weekdays="12pm-11pm" weekend="3pm-11pm" contact="free call 0800 9428787"/>
-    <ContactBox  route="chat" title="Chat online" weekdays="12pm-10pm" weekend="3pm-10pm" contact="whatsup.co.nz/chat"/>
-    <EngageMenu/>
-  </div>
-</div>
-
 </template>
 <script lang="ts">
 import Vue from 'vue'
 
 import TitleImage from '@/components/title-image/TitleImage.vue'
 import EngageMenu from '@/components/engage-menu/EngageMenu.vue'
+import MenuButton from '@/components/menu-button/MenuButton.vue'
 import ContactBox from '~/components/contact-boxes/ContactBox.vue'
 
 export default Vue.extend({
@@ -23,7 +36,8 @@ export default Vue.extend({
   components: {
     TitleImage,
     ContactBox,
-    EngageMenu
+    EngageMenu,
+    MenuButton
   },
 
   head: {
@@ -34,11 +48,15 @@ export default Vue.extend({
 
 <style scoped>
 .container {
-display: grid;
-grid-template-columns: 3fr 2fr;
-margin: auto;
-
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  margin: auto;
 }
 
-
+.hamburger {
+  padding-right: 20px;
+  padding-top: 40px;
+  position: absolute;
+  right: 0;
+}
 </style>
